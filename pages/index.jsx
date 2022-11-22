51e591d43b635799ee9fe8ebe5fa2a59a87a8803
abiamrans <abiamrans@gmail.com>
2022-11-22 20:02:35 +0700

    Feat:LandingPages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,53 +1,193 @@
 import styles from "../styles/Home.module.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
-import { getProfile } from "../src/store/actions/authActions";
-import Header from "../src/component/header";
-import Gamelist from "./gameList";
-import { TextField } from "@mui/material";
-import { Login } from "@mui/icons-material";
+import { Login, Verified, VerifiedUser } from "@mui/icons-material";
 export default function Home() {
   const profile = useSelector((state) => state.authReducer.profile);
   return (
     <>
-    <section>
-    <div className={styles.container}>
-        <div className={styles.content}>
-          <h1>Kapak Wiro C  orp.</h1>
-          <h5>est.2022</h5>
-          <h1>
-            Our Game Page only for fun and make some friends. <br />
-            <span className={styles.span}>Do NOT</span> ruin our{" "}
-            <span className={styles.spans}> GAME ENVIRONMENT. </span>{" "}
-          </h1>
+      <section className={styles.container}>
+        <div className={styles.home}>
+          <a href="" className={styles.bigCard}>
+            <img
+              className={styles.pictureBigCard}
+              src="/assets/COD.jpg"
+              alt="RPS"
+            />
+            <p>Call of Duty: Modern Warfare</p>
+          </a>
+          <div className={styles.GamelistContentRight}>
+            <a href="https://nextjs.org/docs" className={styles.rightContent}>
+              <img
+                className={styles.pictureRightCard}
+                src="/assets/LOS.jpg"
+                alt="RPS"
+              />
+              <p>Last Of Us</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.rightContent}>
+              <img
+                className={styles.pictureRightCard}
+                src="/assets/DS.jpg"
+                alt="RPS"
+              />
+              <p>Dark Souls</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.rightContent}>
+              <img
+                className={styles.pictureRightCard}
+                src="/assets/GOW.jpg"
+                alt="RPS"
+              />
+              <p>God of War</p>
+            </a>
+          </div>
         </div>
-        <img className={styles.Logo} src='./assets/banner.png' alt="" />
+        <div className={styles.GamelistContentBotom}>
+          <a href="https://nextjs.org/docs" className={styles.botomContent}>
+            <img
+              className={styles.pictureBotomCard}
+              src="/assets/TW.jpg"
+              alt="RPS"
+            />
+            <p>The Witcher.</p>
+          </a>
+          <a href="https://nextjs.org/docs" className={styles.botomContent}>
+            <img
+              className={styles.pictureBotomCard}
+              src="/assets/CP.jpeg"
+              alt="RPS"
+            />
+            <p>CyberPunk.</p>
+          </a>
+          <a href="https://nextjs.org/docs" className={styles.botomContent}>
+            <img
+              className={styles.pictureBotomCard}
+              src="/assets/SW.png"
+              alt="RPS"
+            />
+            <p className={styles.gameContent}>Star Wars</p>
+          </a>
         </div>
-    </section>
-    <section className={styles.Gamelist}>
-      <div className={styles.GamelistContent}>
-      <a href="https://nextjs.org/docs" className={styles.cardGamelist}>
-              <h2>Rock Papper Scissors</h2>
-              <img className={styles.picture} src='/assets/banner.png' alt="RPS" />
-              <p>Find in-depth information about Next.js features and API.</p>
+      </section>
+      <section className={styles.Gamelist}>
+        <div className={styles.GamelistContent}>
+          <p className={styles.GamelistTittle}>
+            Most Played by Users <VerifiedUser />{" "}
+          </p>
+          <div className={styles.GamelistUser}>
+            <a href="https://nextjs.org/docs" className={styles.cardGamelist}>
+              <h2>Cyber Punk</h2>
+              <img className={styles.picture} src="/assets/CP.jpeg" alt="RPS" />
+              <p>
+                {" "}
+                USER CHOICE <Verified />
+                <br />
+                <span>Rp. 850.000</span>
+              </p>
+              <button className={styles.buttonGamelist}>Buy Now</button>
             </a>
             <a href="https://nextjs.org/docs" className={styles.cardGamelist}>
-              <h2>Rock Papper Scissors</h2>
-              <img className={styles.picture} src='/assets/banner.png' alt="RPS" />
-              <p>Find in-depth information about Next.js features and API.</p>
+              <h2>The Witcher: Wild Hunt III</h2>
+              <img className={styles.picture} src="/assets/TW.jpg" alt="RPS" />
+              <p>
+                {" "}
+                USER CHOICE <Verified />
+                <br />
+                <span>Rp. 750.000</span>
+              </p>
+              <button className={styles.buttonGamelist}>Buy Now</button>
             </a>
-            <a href="https://nextjs.org/docs" className={styles.cardGamelist}>
-              <h2>Rock Papper Scissors</h2>
-              <img className={styles.picture} src='/assets/banner.png' alt="RPS" />
-              <p>Find in-depth information about Next.js features and API.</p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.rowGame}>
+        <div className={styles.GamelistColGame}>
+          
+          <div className={styles.GamelistCol}>
+          <h1 className={styles.tittle}>Adventures RPG</h1>
+          <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/LOS.jpg"
+                alt="RPS"
+              />
+              <p>Last Of Us<br />Buy Now</p>
             </a>
-      </div>
-
-    </section>
-    <section className={styles.promo}>
-    <div className={styles.input}>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/DS.jpg"
+                alt="RPS"
+              />
+              <p>Dark Souls</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/GOW.jpg"
+                alt="RPS"
+              />
+              <p>God of War</p>
+            </a>
+          </div>
+          <div className={styles.GamelistCol}>
+          <h1 className={styles.tittle}>Developer's Choices</h1>
+          <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/UC.jpg"
+                alt=""
+              />
+              <p>Uncharted</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/AC.png"
+                alt="RPS"
+              />
+              <p>Assasins Creed</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/SM.jpg"
+                alt="RPS"
+              />
+              <p>Spider-Man</p>
+            </a>
+          </div>
+          <div className={styles.GamelistCol}>
+          <h1 className={styles.tittle}>Actions</h1>
+          <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/SR.jpg"
+                alt="RPS"
+              />
+              <p>Sky Rim</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/BF.jpg"
+                alt="RPS"
+              />
+              <p>Battle Field: 2042</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.GamelistColContent}>
+              <img
+                className={styles.GamelistPicture}
+                src="/assets/WZ.jpg"
+                alt="RPS"
+              />
+              <p>Call of Duty WARZONE</p>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className={styles.promo}>
+        <div className={styles.input}>
           <div className={styles.contentContainer}>
             <h1 className={styles.inputContent}>
               Easy{" "}
@@ -58,14 +198,16 @@ export default function Home() {
             </h1>
             <a href="https://nextjs.org/docs" className={styles.card}>
               <h2>Rock Papper Scissors</h2>
-              <img className={styles.picture} src='/assets/banner.png' alt="RPS" />
+              <img
+                className={styles.picture}
+                src="/assets/banner.png"
+                alt="RPS"
+              />
               <p>Find in-depth information about Next.js features and API.</p>
             </a>
           </div>
         </div>
-    </section>
-        
-      
+      </section>
     </>
   );
 }
