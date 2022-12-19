193e2a5410359ec77e1b87a0da6b0970a2b38707
mohamad iqbal tamalaka <iqbaltamalaka01@gmail.com>
2022-12-19 22:53:22 +0800

    dwonload-pdf2

---
+++ src/component/DownloadPdf/buttonStyled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+position: absolute;
+height: 5.5vh;
+font-size: .8em;
+margin-top: 40em;
+margin-left: 22em;
+&:hover {
+    cursor: pointer;
+    color: blue;
+  }
+`
