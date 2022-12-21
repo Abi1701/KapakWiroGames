d96ce75f74d2858cc6515954129980200d3f62c9
abiamrans <abiamrans@gmail.com>
2022-12-21 16:42:23 +0700

    fixed

--- src/component/styled.js
+++ src/component/styled.js
@@ -24,64 +24,79 @@ import styled from "styled-components";
 // `
 
 export const Container = styled.div`
-  background-color: red;
-  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
-  position: relative;
-  overflow: hidden;
-  width: 678px;
-  max-width: 100%;
-  min-height: 400px;
-  height: 90vh;
-  width: 100%;
-  background-size: cover;
+	background-color: red;
+	box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+	position: relative;
+	overflow: hidden;
+	width: 678px;
+	min-width: 1280px;
+	height: 100vh;
+	width: 100%;
+	background-size: cover;
+	@media (max-width: 320px) {
+		height: 1920px;
+	}
+	@media (min-width: 321px) {
+		height: 1620px;
+	}
+	@media (min-width: 424px) {
+		height: 1440px;
+	}
+	@media (min-width: 767px) {
+		height: 1440px;
+	}
+	@media (min-width: 1023px) {
+		height: 790px;
+	}
 `;
 
 export const SignUpContainer = styled.div`
-  position: absolute;
-  top: 0;
-  height: 100%;
-  transition: all 0.6s ease-in-out;
-  left: 0;
-  width: 50%;
-  opacity: 0;
-  z-index: 1;
-  ${(props) =>
-    props.signingIn !== true
-      ? `
+	position: absolute;
+	top: 0;
+	height: 100%;
+	transition: all 0.6s ease-in-out;
+	left: 0;
+	width: 50%;
+	opacity: 0;
+	z-index: 1;
+
+	${(props) =>
+		props.signingIn !== true
+			? `
   transform: translateX(100%);
 	opacity: 1;
 	z-index: 5;
 	`
-      : null}
+			: null}
 `;
 
 export const SignInContainer = styled.div`
-  position: absolute;
-  top: 0;
-  height: 100%;
-  transition: all 0.6s ease-in-out;
-  left: 0;
-  width: 50%;
-  z-index: 2;
-  ${(props) =>
-    props.signingIn !== true ? `transform: translateX(100%);` : null}
+	position: absolute;
+	top: 0;
+	height: 100%;
+	transition: all 0.6s ease-in-out;
+	left: 0;
+	width: 50%;
+	z-index: 2;
+	${(props) =>
+		props.signingIn !== true ? `transform: translateX(100%);` : null}
 `;
 
 export const Form = styled.div`
-  background-color: #434242;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 0 50px;
-  height: 100%;
-  text-align: center;
+	background-color: #434242;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 50px;
+	height: 100%;
+	text-align: center;
 `;
 
 export const Title = styled.h1`
-  font-weight: bold;
-  margin: 0;
-  color:white;
+	font-weight: bold;
+	margin: 0;
+	color: white;
 `;
 
 export const Input = styled.input`
@@ -103,6 +118,31 @@ export const Input = styled.input`
   &;hover {
     box-shadow: #E5DCC3 5px 5px, #C7BEA2 10px 10px;
   }
+  @media (max-width: 320px) {
+		width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 321px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 424px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 767px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 1023px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
 `;
 
 export const Button = styled.button`
@@ -127,91 +167,116 @@ export const Button = styled.button`
   &;hover {
     box-shadow: #E5DCC3 5px 5px, #C7BEA2 10px 10px;
   }
+  @media (max-width: 320px) {
+		width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 321px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 424px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 767px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
+	@media (min-width: 1023px) {
+    width: 70%;
+    height:70px;
+    font-size:24px;
+	}
   `;
 
 export const GhostButton = styled(Button)`
-  background-color: transparent;
-  border-color: #e5dcc3;
-  color: white;
+	background-color: transparent;
+	border-color: #e5dcc3;
+	color: white;
 `;
 
 export const Anchor = styled.a`
-  color: #333;
-  font-size: 14px;
-  text-decoration: none;
-  margin: 15px 0;
+	color: #333;
+	font-size: 14px;
+	text-decoration: none;
+	margin: 15px 0;
 `;
 
 export const OverlayContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  width: 50%;
-  height: 100%;
-  overflow: hidden;
-  transition: transform 0.6s ease-in-out;
-  z-index: 100;
-  ${(props) =>
-    props.signingIn !== true ? `transform: translateX(-100%);` : null}
+	position: absolute;
+	top: 0;
+	left: 50%;
+	width: 50%;
+	height: 100%;
+	overflow: hidden;
+	transition: transform 0.6s ease-in-out;
+	z-index: 100;
+	${(props) =>
+		props.signingIn !== true ? `transform: translateX(-100%);` : null}
 `;
 
 export const Overlay = styled.div`
-  background: black;
-  background: -webkit-linear-gradient(
-    to right,
-    194.68deg,
-    #ffb548 11.27%,
-    #f3af34 90.4%
-  );
-  background: linear-gradient(
-    to right,
-    194.68deg,
-    #ffb548 11.27%,
-    #f3af34 90.4%
-  );
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 0 0;
-  color: #ffffff;
-  position: relative;
-  left: -100%;
-  height: 100%;
-  width: 200%;
-  transform: translateX(0);
-  transition: transform 0.6s ease-in-out;
-  ${(props) =>
-    props.signingIn !== true ? `transform: translateX(50%);` : null}
+	background: black;
+	background: -webkit-linear-gradient(
+		to right,
+		194.68deg,
+		#ffb548 11.27%,
+		#f3af34 90.4%
+	);
+	background: linear-gradient(
+		to right,
+		194.68deg,
+		#ffb548 11.27%,
+		#f3af34 90.4%
+	);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: 0 0;
+	color: #ffffff;
+	position: relative;
+	left: -100%;
+	height: 100%;
+	width: 200%;
+	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+	${(props) =>
+		props.signingIn !== true ? `transform: translateX(50%);` : null}
 `;
 
 export const OverlayPanel = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  text-align: center;
-  top: 0;
-  height: 100%;
-  width: 50%;
-  transform: translateX(0);
-  transition: transform 0.6s ease-in-out;
+	position: absolute;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	text-align: center;
+	top: 0;
+	height: 100%;
+	width: 50%;
+	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
 `;
 
 export const LeftOverlayPanel = styled(OverlayPanel)`
-  transform: translateX(-20%);
-  ${(props) => (props.signingIn !== true ? `transform: translateX(0);` : null)}
+	transform: translateX(-20%);
+	${(props) => (props.signingIn !== true ? `transform: translateX(0);` : null)}
 `;
 
 export const RightOverlayPanel = styled(OverlayPanel)`
-  right: 0;
-  transform: translateX(0);
-  ${(props) =>
-    props.signingIn !== true ? `transform: translateX(20%);` : null}
+	right: 0;
+	transform: translateX(0);
+	${(props) =>
+		props.signingIn !== true ? `transform: translateX(20%);` : null}
 `;
 export const Paragraph = styled.p`
-  font-size: 14px;
-  font-weight: normal;
-  line-height: 20px;
-  letter-spacing: 0.5px;
-  width: 80%;
+	font-size: 14px;
+	font-weight: normal;
+	line-height: 20px;
+	letter-spacing: 0.5px;
+	width: 80%;
 `;
